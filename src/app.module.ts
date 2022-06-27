9e0119181e9e98f33bb833bf555c589703825624
Yimesgen Morka <dochomorka@gmail.com>
2022-06-27 13:32:12 +0300

    configure connection to a database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { KebeleModule } from './kebele/kebele.module';
 import { HealthcareModule } from './healthcare/healthcare.module';
 import { HospitalModule } from './hospital/hospital.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [KebeleModule, HealthcareModule, HospitalModule],
+  imports: [
+    ConfigModule.forRoot(),
+    KebeleModule,
+    HealthcareModule,
+    HospitalModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
