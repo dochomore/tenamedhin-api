4c17177a779ea82050a424ceebb57bce3f2dcfcc
Yimesgen Morka <dochomorka@gmail.com>
2022-06-29 08:01:40 +0300

    added class-validator annotations to credit to validate datatype

--- src/credit/entities/credit.entity.ts
+++ src/credit/entities/credit.entity.ts
@@ -1,3 +1,4 @@
+import { IsNumber, IsString, Min } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -6,26 +7,41 @@ export class Credit {
   creditId: string;
 
   @Column()
+  @IsNumber()
+  @Min(0)
   card: number;
 
+  @Min(0)
   @Column()
+  @IsNumber()
   laboratory: number;
 
+  @Min(0)
   @Column()
+  @IsNumber()
   imaging: number;
 
+  @Min(0)
   @Column()
+  @IsNumber()
   procedure: number;
 
   @Column()
+  @IsNumber()
+  @Min(0)
   medicine: number;
 
   @Column()
+  @IsNumber()
+  @Min(0)
   bed: number;
 
   @Column()
+  @IsNumber()
+  @Min(0)
   others: number;
 
+  @IsString()
   @Column()
   comment: string;
 }
