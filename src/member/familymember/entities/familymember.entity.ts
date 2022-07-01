fa56f5534c3b933b1cbd834a9f9f1b4635688815
Yimesgen Morka <dochomorka@gmail.com>
2022-07-01 14:05:40 +0300

    created FamilyMember Entity

--- src/member/familymember/entities/familymember.entity.ts
+++ src/member/familymember/entities/familymember.entity.ts
@@ -1,1 +1,28 @@
-export class Familymember {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Familymember {
+  @PrimaryGeneratedColumn('uuid')
+  memberId: string;
+
+  @Column({ type: 'timestamptz' })
+  dateOfRegistration: string;
+
+  @Column()
+  fristName: string;
+
+  @Column()
+  fatherName: string;
+
+  @Column()
+  gfName: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  age: number;
+
+  @Column({ type: 'date' })
+  dateOfBirth: string;
+}
