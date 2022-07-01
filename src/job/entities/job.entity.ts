20d9101912374724a778b42b3e9c7afbaa2df92e
Yimesgen Morka <dochomorka@gmail.com>
2022-07-01 09:28:59 +0300

    created new entity Job

--- src/job/entities/job.entity.ts
+++ src/job/entities/job.entity.ts
@@ -1,1 +1,16 @@
-export class Job {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * Tena medhin member profession
+ */
+@Entity()
+export class Job {
+  @PrimaryGeneratedColumn('uuid')
+  jobUUID: string;
+
+  @Column({ type: 'timestamptz', default: new Date() })
+  dateOfCreation: Date;
+
+  @Column()
+  jobName: string;
+}
