5fc9b620f2ee7cf648054d3e0f1c1184969f1b5e
Yimesgen Morka <dochomorka@gmail.com>
2022-07-14 05:08:40 +0300

    setting up dependencies for testing member service

--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Member } from './entities/member.entity';
 import { MemberService } from './member.service';
+
+const mockRepository = () => ({
+  create: jest.fn(),
+});
 
 describe('MemberService', () => {
   let service: MemberService;
+  let repository: Repository<Member>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MemberService],
+      providers: [
+        MemberService,
+        { provide: getRepositoryToken(Member), useFactory: mockRepository },
+      ],
     }).compile();
 
     service = module.get<MemberService>(MemberService);
+    repository = module.get<Repository<Member>>(getRepositoryToken(Member));
   });
 
   it('should be defined', () => {
