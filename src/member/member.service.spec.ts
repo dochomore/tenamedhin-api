ac1057f450d367f805f9d166a04de4b8b5553e19
Yimesgen Morka <dochomorka@gmail.com>
2022-07-15 15:03:03 +0300

    Optimized imports

--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository, RepositoryNotTreeError } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Member } from './entities/member.entity';
 import { MemberService } from './member.service';
 
