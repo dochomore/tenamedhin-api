930e53be9c710deda4a78b9c253252662ba34722
Yimesgen Morka <dochomorka@gmail.com>
2022-07-24 12:02:16 +0300

    implemented POST endpoint to woreda service

--- src/woreda/woreda.service.ts
+++ src/woreda/woreda.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateWoredaDto } from './dto/create-woreda.dto';
 import { UpdateWoredaDto } from './dto/update-woreda.dto';
+import { Woreda } from './entities/woreda.entity';
 
 @Injectable()
 export class WoredaService {
-  create(createWoredaDto: CreateWoredaDto) {
-    return 'This action adds a new woreda';
+  constructor(
+    @InjectRepository(Woreda)
+    private readonly woredaRepository: Repository<Woreda>,
+  ) {}
+
+  async create(createWoredaDto: CreateWoredaDto) {
+    try {
+      const woreda = await this.woredaRepository.create({ ...createWoredaDto });
+      const result = await this.woredaRepository.save(woreda);
+      if (!result) {
+        throw new BadRequestException();
+      }
+      return result;
+    } catch (error) {
+      return new BadRequestException();
+    }
   }
 
   findAll() {
