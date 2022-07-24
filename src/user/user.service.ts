a01d53aac4a97e6e4ef13d26180e00d0025c7b52
Yimesgen Morka <dochomorka@gmail.com>
2022-07-24 11:17:08 +0300

    implemented DELETE endpoint to user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,15 +18,27 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: string) {
+  findOneById(id: string) {
     return `This action returns a #${id} user`;
+  }
+
+  findOneByUsername(username: string) {
+    return `This action returns a #${username} user`;
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} user`;
+  async remove(id: string): Promise<DeleteResult | NotFoundException> {
+    try {
+      const deleteResult = await this.userRepository.delete(id);
+      if (deleteResult.affected === 0) {
+        throw new NotFoundException();
+      }
+      return deleteResult;
+    } catch (error) {
+      return new NotFoundException();
+    }
   }
 }
