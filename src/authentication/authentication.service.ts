e6b7699ec2bf9843598941f34f097939a0c95fcc
Yimesgen Morka <dochomorka@gmail.com>
2022-07-28 03:39:46 +0300

    refactored authentication service

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
-import { CreateAuthenticationDto } from './dto/create-authentication.dto';
-import { UpdateAuthenticationDto } from './dto/update-authentication.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -11,32 +10,12 @@ export class AuthenticationService {
   async validateUser(username: string, password: string) {
     const user = await this.userService.findOneByUsername(username);
     if (user instanceof User) {
-      if (user.password == password) {
+      if (bcrypt.compareSync(password, user.password)) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { password, ...others } = user;
         return others;
       }
     }
     return null;
-  }
-
-  create(createAuthenticationDto: CreateAuthenticationDto) {
-    return 'This action adds a new authentication';
-  }
-
-  findAll() {
-    return `This action returns all authentication`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} authentication`;
-  }
-
-  update(id: number, updateAuthenticationDto: UpdateAuthenticationDto) {
-    return `This action updates a #${id} authentication`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} authentication`;
   }
 }
