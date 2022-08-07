9ca7f5204912f6479391f1a38908133fc9f0cd05
Yimesgen Morka <dochomorka@gmail.com>
2022-08-07 07:14:35 +0300

    refactored auth sevice to return both accessToken and refreshToken

--- src/authentication/strategies/access-token-strategy.ts
+++ src/authentication/strategies/access-token-strategy.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+type JwtPayload = {
+  sub: string;
+  username: string;
+};
+
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: `${process.env.SECRETE_KEY}`,
+      secretOrKey: `${process.env.JWT_ACCESS_SECRET}`,
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+  validate(payload: JwtPayload) {
+    return payload;
   }
 }
