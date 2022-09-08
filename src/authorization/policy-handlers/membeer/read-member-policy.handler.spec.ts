70b9c4e2f83ee10153577df9f048ded13a02d673
Yimesgen Morka <dochomorka@gmail.com>
2022-09-08 10:09:56 +0300

    remove membeer files

--- src/authorization/policy-handlers/membeer/read-member-policy.handler.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReadMemberPolicyHandler } from './read-member-policy.handler';
-
-describe('ReadMemberPolicyHandler', () => {
-  it('should be defined', () => {
-    expect(new ReadMemberPolicyHandler()).toBeDefined();
-  });
-});
