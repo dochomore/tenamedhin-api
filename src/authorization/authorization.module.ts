26d496013e473001c96d6e9e1aa472aa4acef0a1
Yimesgen Morka <dochomorka@gmail.com>
2022-09-13 09:12:23 +0300

    added permission module to module's import statement

--- src/authorization/authorization.module.ts
+++ src/authorization/authorization.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthorizationService } from './authorization.service';
-import { AuthorizationController } from './authorization.controller';
-import { AbilityFactory } from './factories/ability.factory';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Permission } from 'src/permission/entities/permission.entity';
+import { PermissionModule } from 'src/permission/permission.module';
+import { AuthorizationController } from './authorization.controller';
+import { AuthorizationService } from './authorization.service';
+import { AbilityFactory } from './factories/ability.factory';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permission])],
+  imports: [TypeOrmModule.forFeature([Permission]), PermissionModule],
   controllers: [AuthorizationController],
   providers: [AuthorizationService, AbilityFactory],
   exports: [AbilityFactory],
