2706f680fadae6b790844c32ac20dbb49f95992f
Yimesgen Morka <dochomorka@gmail.com>
2022-09-14 10:15:37 +0300

    defined relationship between role and user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,11 +1,9 @@
 import { IsDefined } from 'class-validator';
-import { Permission } from 'src/permission/entities/permission.entity';
 import { Role } from 'src/role/entities/role.entity';
 import {
   Column,
   Entity,
   JoinTable,
-  ManyToMany,
   OneToMany,
   PrimaryGeneratedColumn,
   Relation,
@@ -49,12 +47,6 @@ export class User {
 
   @Column({ nullable: true })
   refreshToken: string;
-
-  @ManyToMany(() => Permission, (permission) => permission.userId, {
-    onDelete: 'CASCADE',
-  })
-  @JoinTable({ name: 'user_with_permission' })
-  permissions: Relation<Permission[]>;
 
   @OneToMany(() => Role, (role) => role.userId)
   @JoinTable({ name: 'user_role' })
