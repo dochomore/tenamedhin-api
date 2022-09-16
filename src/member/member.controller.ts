21a386fb7642c09fc03bf35e02bf9f05bd942a0d
Yimesgen Morka <dochomorka@gmail.com>
2022-09-16 10:49:38 +0300

    added RequirePolicies to each route handler in member.controller.ts

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -12,7 +12,10 @@ import {
 import { AccessTokenGuard } from 'src/authentication/guard/access-token/access-token.guard';
 import { RequirePolicies } from 'src/authorization/decorators/check-policies.decorators';
 import { PoliciesGuard } from 'src/authorization/guards/policies.guard';
+import { CreateMemberPolicyHandler } from 'src/authorization/policy-handlers/member/create/create-member-policy.handler';
+import { DeleteMemberPolicyHandler } from 'src/authorization/policy-handlers/member/delete/delete-member-policy.handler';
 import { ReadMemberPolicyHandler } from 'src/authorization/policy-handlers/member/read/read-member-policy.handler';
+import { UpdateMemberPolicyHandler } from 'src/authorization/policy-handlers/member/update/update-member-policy.handler';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { UpdateMemberDto } from './dto/update-member.dto';
 import { MemberService } from './member.service';
@@ -24,22 +27,25 @@ export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
   @Post()
+  @RequirePolicies(new CreateMemberPolicyHandler())
   create(@Body() createMemberDto: CreateMemberDto) {
     return this.memberService.create(createMemberDto);
   }
 
-  @RequirePolicies(new ReadMemberPolicyHandler())
   @Get()
+  @RequirePolicies(new ReadMemberPolicyHandler())
   findAll() {
     return this.memberService.findAll();
   }
 
   @Get(':id')
+  @RequirePolicies(new ReadMemberPolicyHandler())
   findOne(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.memberService.findOne(id);
   }
 
   @Patch(':id')
+  @RequirePolicies(new UpdateMemberPolicyHandler())
   update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateMemberDto: UpdateMemberDto,
@@ -48,6 +54,7 @@ export class MemberController {
   }
 
   @Delete(':id')
+  @RequirePolicies(new DeleteMemberPolicyHandler())
   remove(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.memberService.remove(id);
   }
