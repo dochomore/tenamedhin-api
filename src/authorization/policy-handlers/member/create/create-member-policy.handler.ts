5238d292f418d85b04334d7e62435a832a6a13ea
Yimesgen Morka <dochomorka@gmail.com>
2022-09-16 10:34:06 +0300

    refactor CreateMemberPolicyHandler handle method

--- src/authorization/policy-handlers/member/create/create-member-policy.handler.ts
+++ src/authorization/policy-handlers/member/create/create-member-policy.handler.ts
@@ -1,1 +1,10 @@
-export class CreateMemberPolicyHandler {}
+import { Action } from 'src/authorization/enums/action';
+import { AppAbility } from 'src/authorization/factories/ability.factory';
+import { IPolicyHandler } from 'src/authorization/interfaces/policy-handler.interface';
+import { Member } from 'src/member/entities/member.entity';
+
+export class CreateMemberPolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility): boolean {
+    return ability.can(Action.CREATE, Member);
+  }
+}
