4bc8827e175e32c617c5a0f36579a6a01717c518
Yimesgen Morka <dochomorka@gmail.com>
2022-09-16 10:37:59 +0300

    refactor ReadMemberPolicyHandler to implements IPolicyHandler

--- src/authorization/policy-handlers/member/read/read-member-policy.handler.ts
+++ src/authorization/policy-handlers/member/read/read-member-policy.handler.ts
@@ -1,8 +1,9 @@
 import { Action } from 'src/authorization/enums/action';
 import { AppAbility } from 'src/authorization/factories/ability.factory';
+import { IPolicyHandler } from 'src/authorization/interfaces/policy-handler.interface';
 import { Member } from 'src/member/entities/member.entity';
 
-export class ReadMemberPolicyHandler {
+export class ReadMemberPolicyHandler implements IPolicyHandler {
   handle(ability: AppAbility): boolean {
     return ability.can(Action.READ, Member);
   }
