3b2892155a8d93d9d013030952a010ab02566019
Yimesgen Morka <dochomorka@gmail.com>
2022-09-16 10:36:26 +0300

    created policy handler for managing member

---
+++ src/authorization/policy-handlers/member/manage/manage-member-policy.handler.spec.ts
@@ -0,0 +1,7 @@
+import { ManageMemberPolicyHandler } from './manage-member-policy.handler';
+
+describe('ManageMemberPolicyHandler', () => {
+  it('should be defined', () => {
+    expect(new ManageMemberPolicyHandler()).toBeDefined();
+  });
+});
