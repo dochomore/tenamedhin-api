5e9f24138110abb8ef48e1a620b66314691ed777
Yimesgen Morka <dochomorka@gmail.com>
2022-11-02 08:21:20 +0300

    implented chicking authorization for reading credit

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,4 @@ export class Constants {}
  * member subject type for casljs authorization
  */
 export const MemberSubject = 'Member';
+export const CreditSubject = 'Credit';
--- src/credit/credit.module.ts
+++ src/credit/credit.module.ts
@@ -3,9 +3,10 @@ import { CreditService } from './credit.service';
 import { CreditController } from './credit.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Credit } from './entities/credit.entity';
+import { AuthorizationModule } from 'src/authorization/authorization.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Credit])],
+  imports: [TypeOrmModule.forFeature([Credit]), AuthorizationModule],
   controllers: [CreditController],
   providers: [CreditService],
 })
