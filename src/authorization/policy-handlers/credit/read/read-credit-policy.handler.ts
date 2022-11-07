93d0bd27b9a1f37571cff67b85d4ec699f1390cf
Yimesgen Morka <dochomorka@gmail.com>
2022-11-07 14:42:55 +0300

    implemented policy handler for reading  credit

--- src/authorization/policy-handlers/credit/read/read-credit-policy.handler.ts
+++ src/authorization/policy-handlers/credit/read/read-credit-policy.handler.ts
@@ -1,1 +1,10 @@
-export class ReadCreditPolicyHandler {}
+import { Action } from 'src/authorization/enums/action';
+import { AppAbility } from 'src/authorization/factories/ability.factory';
+import { IPolicyHandler } from 'src/authorization/interfaces/policy-handler.interface';
+import { CreditSubject } from 'src/constants';
+
+export class ReadCreditPolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility): boolean {
+    return ability.can(Action.READ, CreditSubject);
+  }
+}
