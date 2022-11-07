c8f4241331a912d85e10307c15d61fd5f9deb549
Yimesgen Morka <dochomorka@gmail.com>
2022-11-07 14:41:09 +0300

    implemented policy handler for updating credit

--- src/authorization/policy-handlers/credit/update/update-credit-policy.handler.ts
+++ src/authorization/policy-handlers/credit/update/update-credit-policy.handler.ts
@@ -1,1 +1,10 @@
-export class UpdateCreditPolicyHandler {}
+import { Action } from 'src/authorization/enums/action';
+import { AppAbility } from 'src/authorization/factories/ability.factory';
+import { IPolicyHandler } from 'src/authorization/interfaces/policy-handler.interface';
+import { CreditSubject } from 'src/constants';
+
+export class UpdateCreditPolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility): boolean {
+    return ability.can(Action.UPDATE, CreditSubject);
+  }
+}
