ec523411a011ea89d332516c18b34f13110c6ca3
Yimesgen Morka <dochomorka@gmail.com>
2022-11-07 14:44:49 +0300

    implemented policy handler for deleting credit

--- src/authorization/policy-handlers/credit/delete/delete-credit-policy.handler.ts
+++ src/authorization/policy-handlers/credit/delete/delete-credit-policy.handler.ts
@@ -1,1 +1,10 @@
-export class DeleteCreditPolicyHandler {}
+import { Action } from 'src/authorization/enums/action';
+import { AppAbility } from 'src/authorization/factories/ability.factory';
+import { IPolicyHandler } from 'src/authorization/interfaces/policy-handler.interface';
+import { CreditSubject } from 'src/constants';
+
+export class DeleteCreditPolicyHandler implements IPolicyHandler {
+  handle(ability: AppAbility): boolean {
+    return ability.can(Action.DELETE, CreditSubject);
+  }
+}
